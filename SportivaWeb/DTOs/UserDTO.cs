85bfd602864f33de6e212b8fc43438462d3fe00e
Guille Jabase <guillejabase@gmail.com>
2025-04-21 01:11:50 -0300

    Implementar registro de usuarios.

---
+++ SportivaWeb/DTOs/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportivaWeb.DTOs
+{
+    public class UserDTO
+    {
+        [Required(ErrorMessage = "Email obligatorio.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Contraseña obligatoria.")]
+        public string? Password { get; set; }
+    }
+}
---
+++ SportivaWeb/Services/UsersService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportivaWeb.Data;
+using SportivaWeb.DTOs;
+
+namespace SportivaWeb.Services
+{
+    public class UsersService(AppDbContext context)
+    {
+        public async Task<bool> EmailExistsAsync(string email)
+        {
+            return await context.Users.AnyAsync(u => u.Email == email);
+        }
+
+        public async Task<(bool Success, string? Error)> AddAsync(UserDTO userData)
+        {
+            try
+            {
+                if (await EmailExistsAsync(userData.Email!))
+                {
+                    return (false, "El correo ya está registrado.");
+                }
+
+                context.Users.Add(new()
+                {
+                    Email = userData.Email!,
+                    Password = userData.Password!
+                });
+
+                await context.SaveChangesAsync();
+                return (true, null);
+            }
+            catch
+            {
+                return (false, "Error al registrar el usuario.");
+            }
+        }
+    }
+}
