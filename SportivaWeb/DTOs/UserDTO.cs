ac8cddb8037d7314804a1e251c395795c63a9265
Guillermo Jabase <guillejabase@gmail.com>
2025-04-24 12:52:13 -0300

    vulnerabilidades corregidas

--- SportivaWeb/DTOs/UserDTO.cs
+++ SportivaWeb/DTOs/UserDTO.cs
@@ -5,13 +5,15 @@ namespace SportivaWeb.DTOs
     public class UserDTO
     {
         [Required(ErrorMessage = "Ingrese email.")]
-        [EmailAddress(ErrorMessage = "Email inválido.")]
-        public string? Email { get; set; }
+        [EmailAddress(ErrorMessage = "El email es inválido.")]
+        public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Ingrese contraseña.")]
         [MinLength(8, ErrorMessage = "La contraseña debe tener un mínimo de 8 caracteres.")]
         [MaxLength(16, ErrorMessage = "La contraseña debe tener un máximo de 16 caracteres.")]
         [RegularExpression(@"^[^\s]*$", ErrorMessage = "La contraseña no puede contener espacios.")]
-        public string? Password { get; set; }
+        public string Password { get; set; } = string.Empty;
+
+        public UserDTO() { }
     }
 }
--- SportivaWeb/Models/User.cs
+++ SportivaWeb/Models/User.cs
@@ -5,15 +5,7 @@ namespace SportivaWeb.Models
     public class User
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Email obligatorio.")]
-        [EmailAddress(ErrorMessage = "Email inválido.")]
         public required string Email { get; set; }
-
-        [Required(ErrorMessage = "Contraseña obligatoria.")]
-        [MinLength(8, ErrorMessage = "Mínimo 8 caracteres.")]
-        [MaxLength(32, ErrorMessage = "Máximo 32 caracteres.")]
-        [RegularExpression(@"^[^\s]*$", ErrorMessage = "La contraseña no puede contener espacios.")]
         public required string Password { get; set; }
     }
 }
