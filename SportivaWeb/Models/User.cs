b96e29107adc2e1c98cce92b0391a614ff89bf90
Guille Jabase <guillejabase@gmail.com>
2025-04-24 17:50:21 -0300

    ultimos retoques para la presentacion

--- SportivaWeb/Models/User.cs
+++ SportivaWeb/Models/User.cs
@@ -5,7 +5,17 @@ namespace SportivaWeb.Models
     public class User
     {
         public int Id { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Ingrese email.")]
+        [EmailAddress(ErrorMessage = "El email es inválido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Ingrese contraseña.")]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener un mínimo de 8 caracteres.")]
+        [MaxLength(16, ErrorMessage = "La contraseña debe tener un máximo de 16 caracteres.")]
+        [RegularExpression(@"^[^\s]*$", ErrorMessage = "La contraseña no puede contener espacios.")]
+        public string Password { get; set; } = string.Empty;
+
+        public User() { }
     }
 }
