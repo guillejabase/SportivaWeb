f79bb0fb6382a2ed193ae36186a6f69f6cffe520
Guille Jabase <guillejabase@gmail.com>
2025-04-26 18:43:01 -0300

    avances

--- SportivaWeb/Models/DB/Usuario.cs
+++ SportivaWeb/Models/DB/Usuario.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace SportivaWeb.DTOs
+namespace SportivaWeb.Models.DB
 {
-    public class UserDTO
+    public class Usuario
     {
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "Ingrese email.")]
         [EmailAddress(ErrorMessage = "El email es inválido.")]
         public string Email { get; set; } = string.Empty;
@@ -12,8 +14,6 @@ namespace SportivaWeb.DTOs
         [MinLength(8, ErrorMessage = "La contraseña debe tener un mínimo de 8 caracteres.")]
         [MaxLength(16, ErrorMessage = "La contraseña debe tener un máximo de 16 caracteres.")]
         [RegularExpression(@"^[^\s]*$", ErrorMessage = "La contraseña no puede contener espacios.")]
-        public string Password { get; set; } = string.Empty;
-
-        public UserDTO() { }
+        public string Contra { get; set; } = string.Empty;
     }
 }
---
+++ SportivaWeb/Models/NavItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace SportivaWeb.Models
+{
+    public class NavItem
+    {
+        public required string Href { get; set; }
+        public required Icon Icon { get; set; }
+        public required string Title { get; set; }
+    }
+}
